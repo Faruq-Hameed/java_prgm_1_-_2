ec122ab749b0a571ca91917e8a0c085448a940e3
Faruq Hameed <faruqhameed1@gmail.com>
2025-07-11 20:18:53 +0100

    added part 1: 1,2,3

---
+++ README.md
@@ -0,0 +1,4 @@
+Writing the command `System.out.println("...") can be taxing. In NetBeans, try to write sout on blank line (within main) and press tab
+
+Commands are separated with a semicolon ;. We could, if we wanted to, write almost everything on a single line. However, that would be difficult to understand.
+
---
+++ Sandbox.java
@@ -0,0 +1,18 @@
+
+public class Sandbox {
+
+    /**Main function */
+    public static void main(String[] args) {
+        // Write your program here
+        /* System.out.print("Ada Lovelace - :D");
+        System.out.print(20.0007);
+        System.out.print("Hello world"); */
+        testFunc(); 
+    }
+
+    public static void testFunc (){
+        System.out.println("from tset function"); System.out.println("testing");System.out.println("again");
+    System.out.println();
+    }
+}
+
