3c1756e43ccdcb65bf50ea0dc53d0957d27c5c72
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-07-14 22:25:09 +0100

    added leapyaer class

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LeapYear {
+       public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        int year = reader.nextInt();
+
+        if (year % 100 == 0 && year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
+    }
+}
