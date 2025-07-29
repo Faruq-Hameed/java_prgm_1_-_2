8ec4c0f161f32a123113109114b6e35d2fddce00
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-07-29 08:13:08 +0100

    added more activities

--- Tesr/Main.java
+++ Tesr/Main.java
@@ -1,16 +1,21 @@
 package Tesr;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Book> books = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             // Title: To Kill a Mockingbird
             System.out.println("Title:");
             String title = scanner.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
 
             System.out.println("Pages:");
             int pages = scanner.nextInt();
@@ -18,7 +23,29 @@ public class Main {
             System.out.println("Publication year:");
             int year = scanner.nextInt();
 
+            // create book object
+            Book book = new Book(title, pages, year);
+
+            // add object to the list
+            books.add(book);
         }
 
+        System.out.println("What information will be printed?");
+        String neededInfo = scanner.nextLine();
+        for (Book book : books) {
+            if (neededInfo.equals("everything")) {
+                System.out.println(book.toString());
+            } else if (neededInfo.equals("title")) {
+                System.out.println(book.getTitle());
+            } else if (neededInfo.equals("pages")) {
+                System.out.println(book.getPages());
+            } else if (neededInfo.equals("year")) {
+                System.out.println(book.getYear());
+            } else {
+                System.out.println("Invalid input");
+            }
+
+        }
     }
+
 }
