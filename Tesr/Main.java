6de2a43a605b79726c89706257003835b322b79b
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-07-29 08:07:43 +0100

    added more activities

--- Tesr/Main.java
+++ Tesr/Main.java
@@ -3,9 +3,22 @@ package Tesr;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        while (true) {
+            // Title: To Kill a Mockingbird
+            System.out.println("Title:");
+            String title = scanner.nextLine();
+
+            System.out.println("Pages:");
+            int pages = scanner.nextInt();
+
+            System.out.println("Publication year:");
+            int year = scanner.nextInt();
+
+        }
+
     }
 }
