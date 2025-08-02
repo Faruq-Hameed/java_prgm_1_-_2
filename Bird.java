54b5ec9cb008b45458baa3f87950f11e131daaa1
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-02 22:16:17 +0100

    added more docs

--- Bird.java
+++ Bird.java
@@ -4,4 +4,21 @@ public class Bird {
     public Bird(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object compare) {
+        // check if they reference same location in memory
+        if (this == compare) {
+            return true;
+        }
+
+        // compare if they are from different instances i.e from diff class
+        if (!(compare instanceof Bird)) {
+            return false;
+        }
+
+        // type cast and check for variables equality
+        Bird bird = (Bird) compare;
+        return this.name.equals(bird.name);
+    }
 }
