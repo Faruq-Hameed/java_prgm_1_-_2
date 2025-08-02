e08eb315cb02d5c69aeb822724f86215064e1c2d
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-02 04:27:37 +0100

    updated payment terminal class

--- PaymentTerminal.java
+++ PaymentTerminal.java
@@ -7,20 +7,20 @@ public class PaymentTerminal {
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
-        this.money = 1000.00;
+        this.money = 1000.0;
         this.affordableMeals = 0;
         this.heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
-        double affordableMealsCost = 2.50;
+        double affordableMealsCost = 2.5;
         // increase the amount of cash by the price of an affordable meal and return the
         // change
         if (payment >= affordableMealsCost) {
             this.money = this.money + affordableMealsCost;
             this.affordableMeals = this.affordableMeals + 1;
-            return payment - affordableMealsCost;
+            return (payment - affordableMealsCost);
         }
         // if the payment parameter is not large enough, no meal is sold and the method
         // should return the whole payment
@@ -31,13 +31,13 @@ public class PaymentTerminal {
     public boolean eatAffordably(PaymentCard card) {
         // try to takeout the affordableMeals cost from the card
         // an affordable meal costs 2.50 euros
-        double affordableMealsCost = 2.50;
+        double affordableMealsCost = 2.5;
         // increase the amount of cash by the price of an affordable meal and return the
         // change if the payment card has enough money, the balance of the card is
         // decreased by
         // the price, and the method returns true
         if (card.takeMoney(affordableMealsCost)) {
-            this.money = this.money + affordableMealsCost;
+            // this.money = this.money + affordableMealsCost;
             this.affordableMeals = this.affordableMeals + 1;
             return true;
         }
@@ -46,7 +46,7 @@ public class PaymentTerminal {
     }
 
     public double eatHeartily(double payment) {
-        double heartyMealsCost = 4.30;
+        double heartyMealsCost = 4.3;
         // increase the amount of cash by the price of an hearty meal and return the
         // change
         if (payment >= heartyMealsCost) {
@@ -54,7 +54,7 @@ public class PaymentTerminal {
             this.heartyMeals = this.heartyMeals + 1;
 
             // this.heartyMeals++;
-            return payment - heartyMealsCost;
+            return (payment - heartyMealsCost);
         }
         // if the payment parameter is not large enough, no meal is sold and the method
         // should return the whole payment
@@ -63,23 +63,32 @@ public class PaymentTerminal {
 
     public boolean eatHeartily(PaymentCard card) {
         // a hearty meal costs 4.30 euros
-        double heartyMealsCost = 4.30;
+        double heartyMealsCost = 4.3;
         // increase the amount of cash by the price of an hearty meal and return the
         // change
         if (card.balance() < heartyMealsCost) {
             return false;
         }
-        //if the balance is greater than or equal to the price, the balance is decreased by
+        // if the balance is greater than or equal to the price, the balance is
+        // decreased by
         // the price, and the method returns true
         card.takeMoney(heartyMealsCost);
-        this.money = this.money + heartyMealsCost;
+        // this.money = this.money + heartyMealsCost;
         this.heartyMeals = this.heartyMeals + 1;
         return true;
-    
+
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum < 0) {
+            return;
+        }
+        card.addMoney(sum);
+        this.money += sum;
     }
 
     public String toString() {
-        return "money: " + money + ", number of sold afforable meals: " + affordableMeals
+        return "money: " + money + ", number of sold affordable meals: " + affordableMeals
                 + ", number of sold hearty meals: " + heartyMeals;
     }
 }
