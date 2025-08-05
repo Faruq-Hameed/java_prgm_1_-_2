5aae772f54a0ecdec19eb6cc759aeafb558e4c25
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-05 08:29:27 +0100

    updated suitcase class

--- Suitcase.java
+++ Suitcase.java
@@ -1,6 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
 import java.util.ArrayList;
 
 public class Suitcase {
+
     private ArrayList<Item> items;
     private int maxHeight;
 
@@ -9,24 +20,39 @@ public class Suitcase {
         this.maxHeight = weight;
     }
 
-    public void add(Item item) {
-        int currentSuitWeight = 0; // create a variable to hold the current weight of the suitcase
-        for (Item i : items) { 
-            
-
-        }// loop through the items in the suitcase
-        this.items.add(item); //
+    public void addItem(Item item) {
+        int suitWeight = this.totalWeight();
+        // only add the new item if it weights wont make us to exceed max weight
+        if (suitWeight + item.getWeight() <= this.maxHeight) {
+            this.items.add(item); //
+        }
     }
 
-    // public boolean isEmpty() {
-    //     return this.items.isEmpty(); // check if room is empty return true or false
-    // }
-
-    // public ArrayList<Item> getitems() { // return the list of items in the room
-    //     return this.items;
-    // }
+    public int totalWeight() {
+        int suitWeight = 0; // create a variable to hold the current weight of the suitcase
+        for (Item i : items) {
+            suitWeight += i.getWeight();
+        }
+        return suitWeight;
+    }
 
     public String toString() {
-        return this.items.size() + " items "+ "(" + this.maxHeight + " kg)";
+        int itemsSize = this.items.size();
+        String response = itemsSize + " items (" + this.totalWeight() + " kg)";
+        if (itemsSize == 0) {
+            response = response.replaceFirst("0", "no"); // replace 0 with no
+        }
+        if (itemsSize == 1) {
+            response = response.replace("items", "item");
+        }
+        return response;
+    }
+
+    public void printItems() {
+        System.out.println("The suitcase contains the following items:");
+        for (Item item : items) {
+            System.out.println(item);
+        }
+        System.out.println("Total Weight: " + this.totalWeight() + " kg");
     }
 }
