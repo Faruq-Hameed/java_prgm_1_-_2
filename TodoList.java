533a4ca05c87247547d6529b32252c4ebd54a942
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-06 21:16:42 +0100

    updated list tracker

--- TodoList.java
+++ TodoList.java
@@ -1,3 +1,35 @@
 public class TodoList {
-    
+
+    private ArrayList<String> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void add(String task) {
+        this.tasks.add(task);
+    }
+
+    public void print() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ": " + tasks.get(i));
+        }
+    }
+
+    public int getTaskIndex(String task) {
+        int taskIndex = -1;
+        for (int i = 1; i <= tasks.size(); i++) {
+            if (tasks.get(i).equals(task)) {
+                taskIndex = i;
+            }
+        }
+        return taskIndex;
+    }
+
+    public void remove(int number) {
+        if (tasks.size() >= 1) {
+            tasks.remove(number - 1);
+        }
+    }
+
 }
