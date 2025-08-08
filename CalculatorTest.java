82e752af70d6886f1d743a69e1cc7ca67b243961
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-08 08:21:22 +0100

    added more docs

---
+++ CalculatorTest.java
@@ -0,0 +1,10 @@
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result);
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,17 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        TodoList todoList = new TodoList();
-        UserInterface ui = new UserInterface(todoList, scanner );
-        ui.start();
-        todoList.add("Sample Task 1");
-        todoList.add("Sample Task 2");
-        todoList.print();
-        todoList.remove(1);
-        todoList.print();
-        scanner.close();
+        // Scanner scanner = new Scanner(System.in);
+        // TodoList todoList = new TodoList();
+        // UserInterface ui = new UserInterface(todoList, scanner );
+        // ui.start();
+        // todoList.add("Sample Task 1");
+        // todoList.add("Sample Task 2");
+        // todoList.print();
+        // todoList.remove(1);
+        // todoList.print();
+        // scanner.close();
+        String input = "one\n" + "two\n" +
+                "three\n" + "four\n" +
+                "five\n" + "one\n" +
+                "six\n";
+
+        Scanner reader = new Scanner(input);
+
+        ArrayList<String> read = new ArrayList<>();
+        int count = 0;
+        while (true) {
+            System.out.println("Enter an input: ");
+            String line = reader.nextLine();
+            System.out.println(count +": Input received: " + line);
+            if (read.contains(line)) {
+            System.out.println(count +": Input received: " + line);
+
+                break;
+            }
+            count++;
+
+            read.add(line);
+        }
+
+        System.out.println("Thank you!");
+
+        if (read.contains("six")) {
+            System.out.println("A value that should not have been added to the group was added to it.");
+        }
     }
 }
