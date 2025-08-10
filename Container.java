93fa9efb7c4073d40b4d6587ea89d5ddce605288
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-10 21:49:53 +0100

    added doc about algorithm

---
+++ Container.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Container {
+    private int first;
+    private int second;
+
+    public Container() {
+        this.first = 0;
+        this.second = 0;
+    }
+
+    public int contains() {
+        return this.first + this.second;
+    }
+
+    public void add(int amount) {
+
+        if (this.first + amount <= 100 && amount > 0) {
+            this.first = this.first + amount;
+        }
+        else if (this.first + amount >= 100) {
+            this.first = 100;
+        }
+
+    }
+
+    public void move(int amount) {
+        int amountToAdd = moveFromFirst(amount);
+        System.out.println("amountToAdd is " + amountToAdd);
+        if ((this.second + amountToAdd) < 100) {
+            this.second = this.second + amountToAdd;
+        } else {
+            this.second = 100;
+        }
+        System.out.println("After moving : " + this.second);
+    }
+
+    /** Adjust the first container to the given amount. and set to 0 if negative */
+    public int moveFromFirst(int amount) {
+        int newAmount = this.first - amount;
+        if (newAmount > 0) {
+            this.first = newAmount;
+            return amount;
+        } else {
+            int amountToAdd = this.first;
+            this.first = 0;
+            return amountToAdd;
+        }
+    }
+
+    public void remove(int amount) {
+        // if amount is lesser then remove
+        if (this.second >= amount) {
+            this.second -= amount;
+        } else { // else empty th second container
+            this.second = 0;
+        }
+    }
+
+    // // @Override
+    // public void print() {
+    //     System.out.println("First: " + this.first + "/100");
+    //     System.out.println("Second: " + this.second + "/100");
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     LiquidContainers containers = new LiquidContainers();
+    //     // String input ="add 5 \n" + "add 20 \n" + "";
+    //     while (true) {
+    //         String input = scanner.nextLine();
+    //         if (input.equals("quit")) {
+    //             break;
+    //         }
+    //         String[] inputContent = new String[2];
+    //         inputContent = input.split(" ");
+    //         String command = inputContent[0];
+    //         int amount = Integer.parseInt(inputContent[1]);
+
+    //         switch (command) {
+    //             case "add":
+    //                 containers.add(amount);
+    //                 containers.print();
+    //                 break;
+    //             case "remove":
+    //                 containers.remove(amount);
+    //                 containers.print();
+    //             case "move":
+    //                 containers.move(amount);
+    //                 containers.print();
+    //             default:
+    //                 continue;
+    //             // break;
+    //         }
+    //     }
+    // }
+
+}
--- README.md
+++ README.md
@@ -20,3 +20,14 @@ Whereas in object-oriented programming, the structure of a program is formed by 
 In procedural programming, the state of the program is maintained in variables and tables, and any methods handle only the values provided to them as parameters. The program tells the computer what should happen.
 
 When comparing object-oriented programming with procedural programming, a few essential differences emerge. In object-oriented programming, the state of an object can, in principle, change with any object method, and that change of state can also affect the working of the methods of other objects. As a consequence, other aspects of a program's execution may also be affected since objects can be used in multiple places within the program. In procedural programming, the state of the program is maintained in variables and tables, and any methods handle only the values provided to them as parameters. The program tells the computer what should happen.
+
+### Algorithms
+- Algorithms, precise instructions on how to to accomplish a specific task, are at the core of computer science. In the context of programming, algorithms are typically defined using source code.
+- The concept of efficiency is often associated with algorithms. A programs efficiency, i.e, the computation of required information fast enough, is an integral part of a programs usability. If it took two days for an algorithm designed for forecasting tomorrows weather run, the results wouldn't be very useful! Similarly, a user viewing a TVs program guide won't get any use out of it, if the tv-shows info only loads after the show already ended.
+- The most basic algorithm is the one that simply prints out a message to the user. This is a very simple algorithm, but it is the most basic algorithm. It is also the most common algorithm. It is also the most common algorithm.
+- An algorithm is a procedure for solving a problem or performing a task. An algorithm is a set of well-defined instructions that can be followed to solve a problem or perform a task. 
+- In a more general sense, retrieving and displaying information quickly is an integral part of any applications function. 
+## Sorting information
+If the information (data) giving to a computer, doesn't follow any rules and isn't in order, retrieving that information is taxing for the computer. We need order! Here comes sorting!
+# Selection sort
+Every programmer should be familiar with at least one sorting algorithm (i.e a way to sort an array). We should familiarize ourselves with one "classic" sorting algorithm, the selection sort. 
