4620e40e72ab8066e27b630812e22d0dcbfa8a90
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-11 12:44:47 +0100

    added functions for sorting

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -66,5 +67,22 @@ public class Main {
             strings.set(i, arr[i]);
         }
     }
+
+
+    // public static void sort(int[] array) {
+    //     Arrays.sort(array);
+    // }
+ 
+    // public static void sort(String[] array) {
+    //     Arrays.sort(array);
+    // }
+ 
+    // public static void sortIntegers(ArrayList<Integer> integers) {
+    //     Collections.sort(integers);
+    // }
+ 
+    // public static void sortStrings(ArrayList<String> strings) {
+    //     Collections.sort(strings);
+    // }
 
 }
