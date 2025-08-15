28724298568fe678c00aa56e1e73f1dc8f32143b
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-15 07:33:07 +0100

    added more instructions for searching

--- RecipeSearch.java
+++ RecipeSearch.java
@@ -14,6 +14,9 @@ public class RecipeSearch {
         System.out.println("Commands:");
         System.out.println("list - lists the recipes");
         System.out.println("stop - stops the program");
+        System.out.println("find name - searches recipes by name");
+        System.out.println("find cooking time - searches recipes by cooking time");
+        System.out.println("find ingredient - searches recipes by ingredient");
 
         while (true) {
             System.out.println("Enter command: ");
