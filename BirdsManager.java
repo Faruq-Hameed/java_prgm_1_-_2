d183bce5b76ad272d29e54303992a08379df5c07
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-15 08:37:07 +0100

    compile bird watcher prgrm

--- BirdsManager.java
+++ BirdsManager.java
@@ -12,27 +12,35 @@ public class BirdsManager {
         this.birds.add(bird);
     }
 
-    public void observe(Bird bird){
-        //loop through the list of birds
-        //find the bird in the list and call its observe method. Note bird is an object of Bird class so we are searching for refernce too
-        for(Bird currentBird : birds){
-            //check if the current and the bird points to the same memory location
-            if(bird.equals(currentBird)){
+    public void observe(Bird bird) {
+        // loop through the list of birds
+        // find the bird in the list and call its observe method. Note bird is an object
+        // of Bird class so we are searching for refernce too
+        for (Bird currentBird : birds) {
+            // check if the current and the bird points to the same memory location
+            if (bird.equals(currentBird)) {
                 bird.observe();
-                found = true;
-                // break;
                 return;
             }
         }
-        System.out.println("Not a bird!");
     }
 
-    //print all birds in the list
-    //use the toString method of the Bird class
-    public void printAll(){
-        for(Bird bird : birds){
+    // print all birds in the list
+    // use the toString method of the Bird class
+    public void printAll() {
+        for (Bird bird : birds) {
             System.out.println(bird);
         }
+    }
+
+    public Bird findBird(String name) {
+        for (Bird bird : birds) {
+            if (bird.getName().equals(name)) {
+                bird.observe();
+                return bird;
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -41,7 +49,10 @@ public class BirdsManager {
         while (true) {
             System.out.println("?");
             String command = scanner.nextLine();
-            if(command.equals("Add")){
+            if (command.equals("Quit")) {
+                break;
+            }
+            if (command.equals("Add")) {
                 System.out.println("Name:");
                 String name = scanner.nextLine();
                 System.out.println("Name in Latin:");
@@ -49,10 +60,29 @@ public class BirdsManager {
                 Bird bird = new Bird(name, latinName);
                 manager.addBird(bird);
             }
-            if(command.equals("Observation")){
+            if (command.equals("Observation")) {
                 System.out.println("Bird?");
+                String name = scanner.nextLine();
+                Bird bird = manager.findBird(name);
+                if (bird == null) {
+                    System.out.println("Not a bird!");
+                } else {
+                    bird.observe();
+                }
             }
-
+            if (command.equals("All")) {
+                manager.printAll();
+            }
+            if (command.equals("One")) {
+                System.out.println("Bird?");
+                String name = scanner.nextLine();
+                Bird bird = manager.findBird(name);
+                if (bird == null) {
+                    System.out.println("Not a bird!");
+                } else {
+                    System.out.println(bird);
+                }
+            }
         }
     }
 }
