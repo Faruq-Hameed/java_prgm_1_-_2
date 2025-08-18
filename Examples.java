600e4114420b450c7330b717c823590381feeeda
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-08-18 13:06:03 +0100

    added hasmap prgrm

---
+++ Examples.java
@@ -0,0 +1,33 @@
+public class Examples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // toteuta ohjelmasi tänne
+        HashMap<String, String> postCodes = new HashMap();
+        ArrayList<String> list = new ArrayList<>();
+        int[] arr = { 1, 3 };
+
+        postCodes.put("1", "ibadan");
+        list.add(postCodes.get("1"));
+
+        postCodes.put("2", "akure");
+
+        // System.out.println(postCodes.toString());
+        System.out.println(postCodes);
+        System.out.println(list);
+        System.out.println("Array is : " + arr);
+        postCodes.remove("2");
+        System.out.println(postCodes);
+        System.out.println(postCodes.containsValue("ibadan"));
+        System.out.println(postCodes.containsValue("akure"));
+        System.out.println(postCodes.get("3"));
+        System.out.println(postCodes.remove("3"));
+        postCodes.put("2", "eko");
+        postCodes.put("2", "Eko");
+        postCodes.put("2", "eko");
+
+        System.out.println(postCodes.get("2"));
+        System.out.println(postCodes.remove("2"));
+        System.out.println(postCodes.get("2"));
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+### Learning Java Programming 2 by Mooci University
+[Mooci link](https://java-programming.mooc.fi/part-8/)
+
 ## HashMap 
 A HashMap is, in addition to ArrayList, one of the most widely used of Java's pre-built data structures. The hash map is used whenever data is stored as key-value pairs, where values can be added, retrieved, and deleted using keys. 
+
+If the hash map does not contained the key used for the search, its get method returns a null reference.So removing what with a null existence key also returns null
