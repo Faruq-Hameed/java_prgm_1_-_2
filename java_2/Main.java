f0fa843c8303addf80e511e679d56dfb612b1f16
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-09-07 21:52:26 +0100

    updated for 45mins object comparison in java

--- java_2/Main.java
+++ java_2/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +9,52 @@ public class Main {
         System.out.println(faruq.equals(faruq2));
         System.out.println(faruq.equals(aseda));
         System.out.println(faruq.getClass());
+        System.out.println("-------------------");
+        System.out.println("-------------------");
+        compareBooks();
+        System.out.println("------------Testing for list-------");
+        System.out.println("-------------------");
+        checksForArrayList();
+    }
+
+    public static void compareBooks() {
+        Book book1 = new Book("Rome", 2000, "lorem ipsum");
+        Book book2 = new Book("Rome", 2000, "lorem ipsum");
+        Book book3 = new Book("Rome3", 2003, "lorem ipsum");
+        Book book4 = book1;
+        System.out.println("is 1 and 2 equal " + book1.equals(book2));// true but diff address
+        System.out.println("books address : " + "\n book 1 is " + book1 + "\n book 2 is " + book2 + "\n book 3 is "
+                + book3 + "\n book 4 is " + book4);
+        System.out.println("is 3 and 4 equal " + book3.equals(book4));
+        System.out.println("is 1 and 4 equal " + book1.equals(book4));
+    }
+
+    public static void checksForArrayList() {
+        // ArrayList internally uses equals method to check if an object is already
+        // present in the list
+        // if we don't override equals method in our class, contains method of ArrayList
+        // does not work properly
+        ArrayList<Book> books = new ArrayList<>();
+        Book bookObject = new Book("Book Object", 2000, "...");
+        books.add(bookObject);
+
+        if (books.contains(bookObject)) {
+            System.out.println("Book Object was found.");
+        }
+
+        bookObject = new Book("Book Objects", 2001, ".Hello..");
+
+        if (books.contains(bookObject)) {
+            System.out.println("Book Object was found again.");
+        }
+
+        if (!books.contains(bookObject)) {
+            System.out.println("Book Object was not found.");
+        }
+        System.out.println(books.size());
+        System.out.println(books.get(0));
+        books.set(0, bookObject);
+        System.out.println("books is : " + books);
+
     }
 }
