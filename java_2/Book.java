f4a46d4e83bd0af6fe6c578ac6d3b9ada7531d49
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-10-04 20:07:35 +0100

    added little arraylist doc in part8

---
+++ java_2/Book.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Book {
+    public static void main(String[] args) {
+        String firstName = "faruq";
+        String lastName = "Hameed";
+        System.out.println("is first equals last = " + firstName.equals(lastName));
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add(lastName);
+        strings.add(firstName);
+
+        strings.add(lastName);
+
+        ArrayList<String> stringsB = new ArrayList<>();
+        stringsB.add(lastName);
+        stringsB.add(firstName);
+
+        stringsB.add(lastName);
+        System.out.println(firstName);
+        System.out.println(strings);
+        System.out.println(stringsB);
+        
+        System.out.println(stringsB.equals(strings));
+    }
+}
