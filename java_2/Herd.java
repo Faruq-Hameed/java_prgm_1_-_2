3d9f64f6a711ef8302885a8fc59959646034ae73
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-10-12 09:30:50 +0100

    created herd wihh 2D

---
+++ java_2/Herd.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Herd implements Movable {
+    private List<Movable> herds;
+
+    public Herd() {
+        this.herds = new ArrayList<>();
+    }
+
+    public void addToHerd(Movable movable) {
+        herds.add(movable);
+    }
+
+    public void move(int dx, int dy) {
+        int size = herds.size();
+        int i = 0;
+        while (i < size) {
+            Movable herd = herds.get(i); // get the herd at index i
+            herd.move(dx, dy); // move the herd
+            herds.set(i, herd); // set the herd value to updated values
+            i++;
+        }
+        // for (Movable movable : herds) {
+        // herds.set(dy, movable)
+        // }
+    }
+
+    @Override
+    public String toString() {
+        String list = "";
+        // for (Movable movable : herds) {
+        // list += movable.toString() + "\n";
+        // }
+        int size = herds.size();
+        int i = 0;
+        while (i < size) {
+            if (i + 1 == size) {
+                list += herds.get(i).toString();
+            } else {
+                list += herds.get(i).toString() + "\n";
+            }
+            i++;
+        }
+        return list;
+    }
+}
---
+++ java_2/Organism.java
@@ -0,0 +1,32 @@
+public class Organism implements Movable {
+    private int x;
+    private int y;
+
+    public Organism(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    @Override
+    public String toString() {
+        return "x: " + this.x + "; y: " + this.y;
+    }
+
+    public static void main(String[] args) {
+        Organism organism = new Organism(20, 30);
+
+        System.out.println(organism);
+
+        organism.move(-10, 5);
+
+        System.out.println(organism);
+
+        organism.move(50, 20);
+        System.out.println(organism);
+    }
+}
