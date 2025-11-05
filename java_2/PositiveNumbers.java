a12921f32f48603e5b54fa91974daeb9dd9a4f9d
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-11-05 14:52:42 +0100

    added collect method and example

---
+++ java_2/PositiveNumbers.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // test your method here
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers) {
+        ArrayList<Integer> result = numbers.stream() //
+                .filter(PositiveNumbers::isPositive)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return result;
+    }
+
+    public static boolean isPositive(int number) {
+        return number > 0;
+    }
+}
