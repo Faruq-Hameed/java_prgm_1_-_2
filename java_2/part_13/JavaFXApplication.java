bc0dfdce011f4c6621ac5738c2a56acf918504f7
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-11-27 17:07:06 +0100

    added part 13

--- java_2/part_13/JavaFXApplication.java
+++ java_2/part_13/JavaFXApplication.java
@@ -1,16 +1,29 @@
-package part_13;
-
-// import javafx.application.Application;
-// import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
 public class JavaFXApplication extends Application {
+
     @Override
     public void start(Stage window) {
-        window.setTitle("Hello World!");
+        Button button = new Button("This is a button");
+        Button buttonB = new Button("This is a button");
+        Button buttonC = new Button("This is a button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+        componentGroup.getChildren().add(buttonB);
+        componentGroup.getChildren().add(buttonC);
+
+        Scene scene = new Scene(componentGroup);
+
+        window.setScene(scene);
         window.show();
     }
 
     public static void main(String[] args) {
-        launch(JavaFxApplication.class);
+        launch(JavaFXApplication.class);
     }
 }
